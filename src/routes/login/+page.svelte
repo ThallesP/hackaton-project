f8a4c1eefc43023dedb3a0acd4f265cd5cb96053
Tiago <tgnvsctn@gmail.com>
2024-01-19 21:19:15 -0300

    feat: add web3auth
    add web3auth integration

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,6 +1,31 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { Button } from '@/components/ui/button';
 	import Google from '@/icons/google.svelte';
+	import { web3auth } from '@/userStore';
+	// import 'dotenv/config';
+	// import { Web3Auth } from '@web3auth/modal';
+
+	if ($web3auth?.connected) {
+		goto('/');
+	}
+
+	async function login() {
+		if (!$web3auth) {
+			return;
+		}
+		const pkg = await import('@web3auth/base');
+
+		try {
+			await $web3auth.connectTo(pkg.WALLET_ADAPTERS.OPENLOGIN, {
+				loginProvider: 'google'
+			});
+
+			goto('/');
+		} catch (error) {
+			console.error(error);
+		}
+	}
 </script>
 
 <svelte:head>
@@ -10,7 +35,7 @@
 <main class="flex h-screen flex-col items-center justify-center gap-2">
 	<div class="flex w-96 flex-col gap-2">
 		<h1 class="text-center text-3xl">Login</h1>
-		<Button class="gap-1">
+		<Button class="gap-1" on:click={login}>
 			<Google width={16} height={16} class="stroke-primary" />
 			Conectar com o Google
 		</Button>
